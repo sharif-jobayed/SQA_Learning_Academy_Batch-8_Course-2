f3aabf50f24711a1b42d6cf1a9ffffa966f348a7
sharif-jobayed <sharif.jobayed@gmail.com>
2024-03-09 22:01:21 +0600

    Data processor added

--- IdeaScale_POM_0001/src/test/java/ideaScale_0001/utils/DriverTools.java
+++ IdeaScale_POM_0001/src/test/java/ideaScale_0001/utils/DriverTools.java
@@ -1,4 +1,9 @@
 package ideaScale_0001.utils;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriverTools {
+
+    public static WebDriver driver;
+
 }
--- IdeaScale_POM_0001/src/test/java/ideaScale_0001/utils/ProcessedEnvSettings.java
+++ IdeaScale_POM_0001/src/test/java/ideaScale_0001/utils/ProcessedEnvSettings.java
@@ -1,4 +1,16 @@
 package ideaScale_0001.utils;
 
 public class ProcessedEnvSettings {
+
+    private String FirefoxBrowser;
+    private String ChromeBrowser;
+    private String EdgeBrowser;
+    private int WaitingTime;
+
+
+    public String getFirefoxBrowser(){return FirefoxBrowser;}
+    public String getChromeBrowser(){return ChromeBrowser;}
+    public String getEdgeBrowser(){return EdgeBrowser;}
+    public int getWaitingTime(){return WaitingTime;}
+
 }

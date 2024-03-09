4063990cfc043e778523fc89f96b21da34e91a84
sharif-jobayed <sharif.jobayed@gmail.com>
2024-03-10 04:36:35 +0600

    PageFactory & BasePage are set up

--- ParaBank_POM-0001/src/test/java/com/parabank/parasoft/pages/BasePage.java
+++ ParaBank_POM-0001/src/test/java/com/parabank/parasoft/pages/BasePage.java
@@ -3,10 +3,8 @@ package com.parabank.parasoft.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
 import static com.parabank.parasoft.utils.DriverTools.driver;
 import static com.parabank.parasoft.utils.DriverTools.getXWait;
 
